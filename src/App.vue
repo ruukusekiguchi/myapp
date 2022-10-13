0699e6d163276ee1afb25f1b8354f7f635c1e13a
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2022-10-13 13:07:28 +0900

    チャット風作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+            <div class="chat_list">
+                <div v-for="m in message" :key="m.key">
+                    <div :class="m.style">{{m.id}}さん「{{m.message}}」</div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="my_chat">
+                <input v-model="my_message" type="text" placeholder="あなたのチャット">
+                <button @click="mySend">送信する</button>
+            </div>
+            <div class="other_chat">
+                <input v-model="other_message" type="text" placeholder="相手のチャット">
+                <button @click="otherSend">送信する</button>
+            </div>
+        </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "app",
+  data(){
+        return{
+            message: [],
+        }
+    },
+    methods:{
+        mySend(){
+            this.message.push({id:1, style:'my', message:this.my_message});
+            this.my_message = ''
+        },
+        otherSend(){
+            this.message.push({id:2, style:'other', message:this.other_message});
+            this.other_message = ''
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.my{
+    color:red;
+
+}
+.other{
+    color: blue;
 }
 </style>
